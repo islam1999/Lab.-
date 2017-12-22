1eb8bbdaecd2bc82614a804ae32278643a05d7c7
islam1999 <31537821+islam1999@users.noreply.github.com>
2017-12-22 12:32:04 +0300

    Update ls.c

--- ls.c
+++ ls.c
@@ -1,125 +1,73 @@
 #define _GNU_SOURCE
-
-
-
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <sys/stat.h>
-
 #include <unistd.h>
-
 #include <dirent.h>
 
+int check_path(const char *path)
+    {
+        struct stat st;
+        if(stat(path, &st) == -1) {
+        printf("unable to open %s\n", path);
+    return 0;
+    }
+    return 1;
+    }
 
 
-int check_path(const char *path) {
 
-struct stat st;
+int is_dir(const char *path)
+    {
+    struct stat st;
+    stat(path, &st);
+    return (st.st_mode & S_IFMT) & S_IFDIR;
+    }
 
-if(stat(path, &st) == -1) {
 
-printf("unable to open %s\n", path);
 
-return 0;
-
+void ls(const char *path) 
+{
+    if(!check_path(path))
+    return;
+    
+    if(is_dir(path)) 
+    {
+    DIR *dir = opendir(path);
+    struct dirent *entry;
+    for(entry = readdir(dir); entry != NULL; entry = readdir(dir)) 
+    {
+    printf("%s\t", entry->d_name);
+    }
+    closedir(dir);
+    } 
+    
+    else 
+    {
+    printf("%s\t", path);
+    }
 }
-
-
-
-return 1;
-
-}
-
-
-
-int is_dir(const char *path) {
-
-struct stat st;
-
-stat(path, &st);
-
-return (st.st_mode & S_IFMT) & S_IFDIR;
-
-}
-
-
-
-void ls(const char *path) {
-
-if(!check_path(path))
-
-return;
-
-
-
-if(is_dir(path)) {
-
-DIR *dir = opendir(path);
-
-
-
-struct dirent *entry;
-
-
-
-for(entry = readdir(dir); entry != NULL; entry = readdir(dir)) {
-
-printf("%s\t", entry->d_name);
-
-}
-
-
-
-closedir(dir);
-
-} else {
-
-printf("%s\t", path);
-
-}
-
-}
-
 
 
 int main(int argc, char *argv[]) {
 
-if(argc == 1) {
+    if(argc == 1) 
+    {
+        char *cur_dir = getcwd(NULL, 0);
+        ls(cur_dir);
+        free(cur_dir);
+        printf("\n");
+        return 0;
+    }
 
-    char *cur_dir = getcwd(NULL, 0);
-
-    ls(cur_dir);
-
-    free(cur_dir);
-
-    printf("\n");
-
-    return 0;
-
-}
-
-else
-
-{
-
-int i;
-
-for(i = 1; i < argc; i++) {
-
-ls(argv[i]);
-
-}
-
-}
-
-
-
+     else
+    {
+    int i;
+    for(i = 1; i < argc; i++) 
+        {
+          ls(argv[i]);
+        }
+    }
 printf("\n");
-
-
-
 return 0;
-
 }
